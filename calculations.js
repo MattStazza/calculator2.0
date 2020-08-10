d96e7c0959766ad7599f8b8c45063d98a9f4b980
Matt <matthewstazza@yahoo.com.au>
2020-08-10 11:54:23 +0800

    calculations are working

--- calculations.js
+++ calculations.js
@@ -2,23 +2,23 @@
 
 
 
-//Reference to the Calculator <div> in the HTML Document.
+// Reference to the Calculator <div> in the HTML Document.
 const calculator = document.querySelector('.calculator');
 
-//Reference to the Keys <div> in the HTML Document.
-//All the buttons are children of the .calculator__keys <div>.
+// Reference to the Keys <div> in the HTML Document.
+// All the buttons are children of the .calculator__keys <div>.
 const keys = calculator.querySelector('.calculator__keys');
 
-//Reference to the Display <div> in the HTML Document.
+// Reference to the Display <div> in the HTML Document.
 const display = calculator.querySelector('.calculator__display');
 
 
-//The change 2
 
-//Listen for all keypresses.
+
+// Listen for all keypresses.
 keys.addEventListener('click', e => {
 
-    //Loops through each key in the 'calculator__keys' <div> and
+    // Loops through each key in the 'calculator__keys' <div> and
     // checks to see if it is a button.
     if (e.target.matches('button')) {
         
@@ -29,101 +29,138 @@ keys.addEventListener('click', e => {
         const keyContent = key.textContent;
         const displayedNum = display.textContent;
 
+        // Reference to the custom attribute: 'previousKeyType'
+        const previousKeyType = calculator.dataset.previousKeyType; 
 
-        //DETERMINING THE TYPE OF KEY THAT WAS PRESSED.-------------------------|
+
+        // DETERMINING THE TYPE OF KEY THAT WAS PRESSED.-------------------------|
         
-        //Determine's Number keys
+        // NUMBER KEYS:
         if(!action){
-            console.log('number key bro...');
+
+            // Updating previousKeyType to number key.
+            calculator.dataset.previousKeyType = 'number';
+
+            // Is the display currently equal to '0' OR was the previous key pressed an operator key?
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+
+              // Updates display to the number pressed.
+              display.textContent = keyContent;
+
+              // Else (The user presses a number key and the display is NOT currently '0'):
+            } else {
+
+              // Add the pressed number to the currently displayed number.
+              display.textContent = displayedNum + keyContent;
+            }
+
+            // Remove .is-depressed class from all keys
+            Array.from(key.parentNode.children)
+                .forEach(k => k.classList.remove('is-depressed'))
         }
 
 
-        //Determine's Operator keys
+
+
+        // OPERATOR KEYS:
         if(
             action === 'add' ||
             action === 'subtract' ||
             action === 'multiply' ||
             action === 'divide' 
         )   {
-            console.log('operator key yo!');
 
-            //Adding a style (from the CSS)...
-            //...to indicate that the operator key is pressed.
+            //  Updating previousKeyType to an operator key.
+            calculator.dataset.previousKeyType = 'operator';
+
+            // Adding a style (from the CSS)...
+            // ...to indicate that the operator key is pressed.
             key.classList.add('is-depressed');
 
-            //Add custom attribute:
-            calculator.dataset.previousKeyType = 'operator';
+            // Storing the displayed number as the first value.
+            calculator.dataset.firstValue = displayedNum;
+
+            // Storing the operator (+, -, X, /, * etc)
+            calculator.dataset.operator = action;
         }
 
 
-        //Other Keys
-            //Determine's Decimal key:
+
+
+            // DECIMAL KEY:
             if(action === 'decimal'){
-                console.log('decimal key');
+                
+                // Updating previousKeyType to decimal key.
+                calculator.dataset.previousKeyType = 'decimal';
+
+                // Do nothing if string has a dot
+                if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.';
+                } else if (previousKeyType === 'operator'){
+                    display.textContent = '0';
+                }
             }
 
-            //Determine's Clear key
+
+
+
+            // CLEAR KEY:
             if(action === 'clear'){
-                console.log('clear key!');
+
+                //Updating previousKeyType to clear key.
+                calculator.dataset.previousKeyType = 'clear';
+
             }
 
-            //Determine's Calculate key
+
+            
+
+  //-CALCULATIONS--------------------------------------------------------------|
+        
+        const calculate = (n1, operator, n2) => {
+            
+            // Perform calculation and return calculated value.
+            let result = '';
+            
+            if (operator === 'add') {
+                result = parseFloat(n1) + parseFloat(n2); 
+            } else if (operator === 'subtract') {
+                result = parseFloat(n1) - parseFloat(n2); 
+            } else if (operator === 'multiply') {
+                result = parseFloat(n1) * parseFloat(n2);
+            } else if (operator === 'divide') {
+                result = parseFloat(n1) / parseFloat(n2);
+            }
+
+            return result;
+        }
+    //-------------------------------------------------------------------------|
+           
+    
+
+    
+        // CALUCLATE (EQUALS) KEY:
             if(action === 'calculate'){
-                console.log('equal key!');
 
-                //When the user presses the [=] button...
-                //...The current number in the display is set to the 'secondValue' variable.
+                // Updating previousKeyType to calculate key.
+                calculator.dataset.previousKeyType = 'calcuate';
+
+                // Stores the first value.
+                const firstValue = calculator.dataset.firstValue;
+
+                // Stores the operator action.
+                const operator = calculator.dataset.operator;
+
+                // When the user presses the [=] button...
+                // ...The current number in the display is set to the 'secondValue' variable.
                 const secondValue = displayedNum;
+
+                // Updates the display to the result.
+                display.textContent = calculate(firstValue, operator, secondValue);
             }
-        //-----------------------------------------------------------------------|
-
-
-        
-
-        //UPDATING THE DISPLAY---------------------------------------------------|
-
-        //Reference to the custom attribute: 'previousKeyType'
-        const previousKeyType = calculator.dataset.previousKeyType;
-
-        //Pressing a NUMBER KEY:
-        //Is it a Number Key/button?
-        if (!action) {
-
-            //Is the display currently equal to '0' OR was the previous key pressed an operator key?
-            if (displayedNum === '0' || previousKeyType === 'operator') {
-
-              //Updates display to the number pressed.
-              display.textContent = keyContent;
-
-              //Else (The user presses a number key and the display is NOT currently '0'):
-            } else {
-
-              //Add the pressed number to the currently displayed number.
-              display.textContent = displayedNum + keyContent;
-            }
-          }
-
-        //Remove .is-depressed class from all keys
-        Array.from(key.parentNode.children)
-            .forEach(k => k.classList.remove('is-depressed'))
-
-
-
-        //Pressing the DECIMAL KEY:
-        //Is it a Decimal Key?
-        if (action === 'decimal') {
-
-            //Adds a '.' to the display
-            display.textContent = displayedNum + '.';
-          }
-
-          
-
-        //--------------------------------------------------------------------------|
-        
-
 
     }
+
 });
 
 
