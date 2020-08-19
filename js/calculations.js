5e883e82b7551a5e7cd361716d0aa86b04677064
Matt <matthewstazza@yahoo.com.au>
2020-08-19 14:56:47 +0800

    Reverted back to before refactored Number keys because it broke the calculator :(

--- js/calculations.js
+++ js/calculations.js
@@ -50,21 +50,20 @@ keys.addEventListener("click", (e) => {
     // DETERMINING THE TYPE OF KEY THAT WAS PRESSED.-------------------------|
 
     // NUMBER KEYS:
-    const createResultString = () => {
-      // Variables required are:
-      // 1. keyContent
-      // 2. displayedNum
-      // 3. previousKeyType
-      // 4. action
-      if (!action) {
-        // Is the display currently equal to '0' OR was the previous key pressed an operator key?
-        return displayedNum === "0" ||
-          previousKeyType === "operator" ||
-          previousKeyType === "calculate"
-          ? // Updates display to the number pressed.
-            keyContent
-          : // Add the pressed number to the currently displayed number.
-            displayedNum + keyContent;
+    if (!action) {
+      // Is the display currently equal to '0' OR was the previous key pressed an operator key?
+      if (
+        displayedNum === "0" ||
+        previousKeyType === "operator" ||
+        previousKeyType === "calculate"
+      ) {
+        // Updates display to the number pressed.
+        display.textContent = keyContent;
+
+        // Else (The user presses a number key and the display is NOT currently '0'):
+      } else {
+        // Add the pressed number to the currently displayed number.
+        display.textContent = displayedNum + keyContent;
       }
 
       // Remove .is-depressed class from all keys
@@ -73,130 +72,130 @@ keys.addEventListener("click", (e) => {
       );
 
       calculator.dataset.previousKeyType = "number";
-    };
-  }
+    }
 
-  // ------------|
+    // ------------|
 
-  // OPERATOR KEYS:
-  if (
-    action === "add" ||
-    action === "subtract" ||
-    action === "multiply" ||
-    action === "divide"
-  ) {
-    const firstValue = calculator.dataset.firstValue;
-    const operator = calculator.dataset.operator;
-    const secondValue = displayedNum;
-
-    // [Note: the secondValue isn't needed because it always exists in the display.]
+    // OPERATOR KEYS:
     if (
-      firstValue &&
-      operator &&
-      previousKeyType !== "operator" &&
-      previousKeyType !== "calculate"
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
     ) {
-      const calcValue = calculate(firstValue, operator, secondValue);
-      display.textContent = calcValue;
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
 
-      // Update calculated value as firstValue
-      calculator.dataset.firstValue = calcValue;
-    } else {
-      // If there are no calculations, set displayedNum as the firstValue.
-      calculator.dataset.firstValue = displayedNum;
-    }
+      // [Note: the secondValue isn't needed because it always exists in the display.]
+      if (
+        firstValue &&
+        operator &&
+        previousKeyType !== "operator" &&
+        previousKeyType !== "calculate"
+      ) {
+        const calcValue = calculate(firstValue, operator, secondValue);
+        display.textContent = calcValue;
 
-    // Remove .is-depressed class from all keys
-    Array.from(key.parentNode.children).forEach((k) =>
-      k.classList.remove("is-depressed")
-    );
-
-    key.classList.add("is-depressed");
-    calculator.dataset.previousKeyType = "operator";
-    calculator.dataset.operator = action;
-  }
-
-  // ------------|
-
-  // DECIMAL KEY:
-  if (action === "decimal") {
-    // Do nothing if string has a dot already.
-    if (!displayedNum.includes(".")) {
-      display.textContent = displayedNum + ".";
-
-      // Display '0.' if user presses the decimal key AFTER an operator key
-      // OR calculate key.
-    } else if (
-      previousKeyType === "operator" ||
-      previousKeyType === "calculate"
-    ) {
-      display.textContent = "0.";
-    }
-
-    // Updating previousKeyType to decimal key.
-    calculator.dataset.previousKeyType = "decimal";
-  }
-
-  // ----------|
-
-  // (NOT) CLEAR KEY [CE]:
-  if (action !== "clear") {
-    // Reference to the clear button and data.
-    const clearButton = calculator.querySelector("[data-action=clear]");
-
-    // Updating the Clear Button's text to "CE".
-    clearButton.textContent = "CE";
-  }
-
-  //-----------|
-
-  // CLEAR KEY [AC]
-  if (action === "clear") {
-    // Reset the Calculator to its initial state
-    if ((key.textContent = "AC")) {
-      calculator.dataset.firstValue = "";
-      calculator.dataset.modValue = "";
-      calculator.dataset.operator = "";
-      calculator.dataset.previousKeyType = "";
-    } else {
-      // Updating the Clear Button's text to "AC".
-      key.textContent = "AC";
-    }
-
-    // Sets display to 0.
-    display.textContent = 0;
-
-    // Remove .is-depressed class from all keys
-    Array.from(key.parentNode.children).forEach((k) =>
-      k.classList.remove("is-depressed")
-    );
-
-    //Updating previousKeyType to clear key.
-    calculator.dataset.previousKeyType = "clear";
-  }
-
-  // ------------|
-
-  // CALUCLATE (EQUALS) KEY:
-  if (action === "calculate") {
-    // Sets firstValue, operator and secondValue.
-    let firstValue = calculator.dataset.firstValue;
-    const operator = calculator.dataset.operator;
-    const secondValue = displayedNum;
-
-    if (firstValue) {
-      if (previousKeyType === "calculate") {
-        firstValue = displayedNum;
-        secondValue = calculator.dataset.modValue;
+        // Update calculated value as firstValue
+        calculator.dataset.firstValue = calcValue;
+      } else {
+        // If there are no calculations, set displayedNum as the firstValue.
+        calculator.dataset.firstValue = displayedNum;
       }
-      // Updates the display to the result.
-      display.textContent = calculate(firstValue, operator, secondValue);
+
+      // Remove .is-depressed class from all keys
+      Array.from(key.parentNode.children).forEach((k) =>
+        k.classList.remove("is-depressed")
+      );
+
+      key.classList.add("is-depressed");
+      calculator.dataset.previousKeyType = "operator";
+      calculator.dataset.operator = action;
     }
 
-    // Set ModValue
-    calculator.dataset.modVlaue = secondValue;
+    // ------------|
 
-    // Updating previousKeyType to calculate key.
-    calculator.dataset.previousKeyType = "calculate";
+    // DECIMAL KEY:
+    if (action === "decimal") {
+      // Do nothing if string has a dot already.
+      if (!displayedNum.includes(".")) {
+        display.textContent = displayedNum + ".";
+
+        // Display '0.' if user presses the decimal key AFTER an operator key
+        // OR calculate key.
+      } else if (
+        previousKeyType === "operator" ||
+        previousKeyType === "calculate"
+      ) {
+        display.textContent = "0.";
+      }
+
+      // Updating previousKeyType to decimal key.
+      calculator.dataset.previousKeyType = "decimal";
+    }
+
+    // ----------|
+
+    // (NOT) CLEAR KEY [CE]:
+    if (action !== "clear") {
+      // Reference to the clear button and data.
+      const clearButton = calculator.querySelector("[data-action=clear]");
+
+      // Updating the Clear Button's text to "CE".
+      clearButton.textContent = "CE";
+    }
+
+    //-----------|
+
+    // CLEAR KEY [AC]
+    if (action === "clear") {
+      // Reset the Calculator to its initial state
+      if ((key.textContent = "AC")) {
+        calculator.dataset.firstValue = "";
+        calculator.dataset.modValue = "";
+        calculator.dataset.operator = "";
+        calculator.dataset.previousKeyType = "";
+      } else {
+        // Updating the Clear Button's text to "AC".
+        key.textContent = "AC";
+      }
+
+      // Sets display to 0.
+      display.textContent = 0;
+
+      // Remove .is-depressed class from all keys
+      Array.from(key.parentNode.children).forEach((k) =>
+        k.classList.remove("is-depressed")
+      );
+
+      //Updating previousKeyType to clear key.
+      calculator.dataset.previousKeyType = "clear";
+    }
+
+    // ------------|
+
+    // CALUCLATE (EQUALS) KEY:
+    if (action === "calculate") {
+      // Sets firstValue, operator and secondValue.
+      let firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      if (firstValue) {
+        if (previousKeyType === "calculate") {
+          firstValue = displayedNum;
+          secondValue = calculator.dataset.modValue;
+        }
+        // Updates the display to the result.
+        display.textContent = calculate(firstValue, operator, secondValue);
+      }
+
+      // Set ModValue
+      calculator.dataset.modVlaue = secondValue;
+
+      // Updating previousKeyType to calculate key.
+      calculator.dataset.previousKeyType = "calculate";
+    }
   }
 });
